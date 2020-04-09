607572caed65b50e4bb46ce36952d7e137c466c0
Mehmet Aydogdu <140202062@kocaeli.edu.tr>
2020-04-09 21:24:46 +0300

    precompile assets

--- lib/jobbr/engine.rb
+++ lib/jobbr/engine.rb
@@ -1,10 +1,10 @@
 require 'jobbr/logger'
-
 module Jobbr
   class Engine < ::Rails::Engine
-
     isolate_namespace Jobbr
-
+    initializer "jobbr.assets.precompile" do |app|
+      app.config.assets.precompile += %w( application.js application.css )
+    end
     initializer 'jobbr.action_controller' do |app|
       ActiveSupport.on_load :action_controller do
         helper Jobbr::ApplicationHelper
